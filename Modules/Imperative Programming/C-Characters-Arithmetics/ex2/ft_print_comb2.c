c0d1ef6767b26b9b721c64c43e0ff366b17a21be
Zaid Tawalbeh <tzearthandair@gmail.com>
2025-09-24 08:44:16 +0300

    Imperative Programming Part II - C Characters Arithmetics

---
+++ Modules/Imperative Programming/C-Characters-Arithmetics/ex2/ft_print_comb2.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ztawalbe <ztawalbe@learner.42.tech>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/11 07:27:46 by ztawalbe          #+#    #+#             */
+/*   Updated: 2025/09/11 07:28:17 by ztawalbe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	print_pair(int a, int b)
+{
+	ft_putchar(a / 10 + '0');
+	ft_putchar(a % 10 + '0');
+	ft_putchar(' ');
+	ft_putchar(b / 10 + '0');
+	ft_putchar(b % 10 + '0');
+	if (!(a == 98 && b == 99))
+		write(1, ", ", 2);
+}
+
+void	ft_print_comb2(void)
+{
+	int	a;
+	int	b;
+
+	a = 0;
+	while (a <= 98)
+	{
+		b = a + 1;
+		while (b <= 99)
+			print_pair(a, b++);
+		a++;
+	}
+}
