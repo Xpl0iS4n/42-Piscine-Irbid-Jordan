237f90e97444a200ea34dd0431550216b84ef942
Zaid Tawalbeh <tzearthandair@gmail.com>
2025-09-24 09:05:30 +0300

    Memory Management Part III - C Function Pointers

---
+++ Modules/Memory_Management/C-Function-Pointers/ex1/ft_map.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_map.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ztawalbe <ztawalbe@learner.42.tech>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/14 07:43:27 by ztawalbe          #+#    #+#             */
+/*   Updated: 2025/09/14 08:10:22 by ztawalbe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	*ft_map(int *tab, int length, int (*f)(int))
+{
+	int	*res;
+	int	i;
+
+	res = (int *)malloc(sizeof(int) * length);
+	if (!res)
+		return (0);
+	i = 0;
+	while (i < length)
+	{
+		res[i] = f(tab[i]);
+		i++;
+	}
+	return (res);
+}
