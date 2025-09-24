7858c1796a01047b6d23362f2f0ea573792dd2be
Zaid Tawalbeh <tzearthandair@gmail.com>
2025-09-24 08:55:34 +0300

    Imperative Programming Part III - C Algorithmics Fundamentals

---
+++ Modules/Imperative_Programming/C-Algorithmics-Fundamentals/ex2/ft_iterative_power.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ztawalbe <ztawalbe@learner.42.tech>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/12 22:01:51 by ztawalbe          #+#    #+#             */
+/*   Updated: 2025/09/14 07:20:43 by ztawalbe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_iterative_power(int nb, int power)
+{
+	int	result;
+
+	result = 1;
+	if (power < 0)
+		return (0);
+	if (power == 0)
+		return (1);
+	while (power > 0)
+	{
+		result *= nb;
+		power--;
+	}
+	return (result);
+}
