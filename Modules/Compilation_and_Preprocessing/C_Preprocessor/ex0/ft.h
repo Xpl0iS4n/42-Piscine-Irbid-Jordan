2c725b91976b491de2e5c196e66ec4bf3490726d
Zaid Tawalbeh <tzearthandair@gmail.com>
2025-10-01 15:49:08 +0300

    Compilation and Preprocessing Part I - C Preprocessor

---
+++ Modules/Compilation_and_Preprocessing/C_Preprocessor/ex0/ft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ztawalbe <ztawalbe@learner.42.tech>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/01 12:05:40 by ztawalbe          #+#    #+#             */
+/*   Updated: 2025/10/01 12:06:31 by ztawalbe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+# define FT_H
+
+void	ft_putchar(char c);
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
+
+#endif
